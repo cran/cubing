0a55b06ff8305b45b424ec23b7ad39401c8b2499
Alec Stephenson <alec_stephenson@hotmail.com>
2018-04-14 08:26:11 +0000

    version 1.0-4

--- src/cubing_init.c
+++ src/cubing_init.c
@@ -1,11 +1,6 @@
-#include <R.h>
-#include <Rinternals.h>
-#include <stdlib.h> // for NULL
+#include "header.h"
+#include <stdlib.h>
 #include <R_ext/Rdynload.h>
-
-extern void kociemba1(void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *);
-extern void twistflip1(void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *);
-extern void zemtwist1(void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *);
 
 static const R_CMethodDef CEntries[] = {
     {"kociemba1",  (DL_FUNC) &kociemba1,  18},
---
+++ src/header.h
@@ -0,0 +1,29 @@
+  
+void kociemba1(int *mt_co, int *mt_eo, int *mt_e4b, 
+              int *mt_cp, int *mt_e4t, int *mt_eMSp, int *mt_eEp,
+              int *pt_eoXeEb, int *pt_coXeEb, 
+              int *pt_cpXeEp, int *pt_eMSpXeEp,
+              int *tt_eMSp, int *tt_eMSp_Map,
+              int *maxDepth, int *verbose,
+              int *search, int *depthtotal, int *depthA);
+
+int kociemba2(int *search, int depth1, int maxDepth,
+              int *mt_cp, int *mt_e4t, int *mt_eMSp, int *mt_eEp,
+              int *pt_cpXeEp, int *pt_eMSpXeEp,
+              int *tt_eMSp, int *tt_eMSp_Map);
+
+void zemtwist1(int *mt_co, int *mt_eo, int *mt_e4b, 
+              int *mt_cp, int *mt_e4t, int *mt_eMSp, int *mt_eEp,
+              int *pt_eoXeEb, 
+              int *pt_cpXeEp, int *pt_eMSpXeEp,
+              int *tt_eMSp, int *tt_eMSp_Map,
+              int *maxDepth, int *verbose,
+              int *search, int *depthtotal, int *depthA);
+
+void twistflip1(int *mt_co, int *mt_eo, int *mt_e4b, 
+              int *mt_cp, int *mt_e4t, int *mt_eMSp, int *mt_eEp,
+              int *pt_eEb, 
+              int *pt_cpXeEp, int *pt_eMSpXeEp,
+              int *tt_eMSp, int *tt_eMSp_Map,
+              int *maxDepth, int *verbose,
+              int *search, int *depthtotal, int *depthA);
